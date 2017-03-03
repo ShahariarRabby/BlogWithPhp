12e460970969b0e93796cd6b288f11e99728b1a0
shahariar Rabby <shahariarrabby@gmail.com>
2017-03-04 02:56:57 +0600

    Database connection done

---
+++ include/db.php
@@ -0,0 +1,23 @@
+<?php
+
+//$connection = mysqli_connect('localhost' , 'root' , '' ,'cms' );
+$servername = 
+$username = 
+$password = 
+$dbname = 
+
+$connection = mysqli_connect($servername, $username, $password, $dbname);
+
+
+mysqli_errno($connection);
+if($connection){
+
+}else{
+	die("Try agin letter");
+}
+
+
+
+?>
+
+
